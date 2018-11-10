9af5b7195bed3dcd71eecde1a7a553d48a724da8
AnonymousSB <samuelbrecher93@gmail.com>
2018-11-09 23:17:49 -1000

    Update RepeatedString Challenge

--- Algorithms/Implementation/RepeatedString/index.js
+++ Algorithms/Implementation/RepeatedString/index.js
@@ -1,14 +1,21 @@
+function countA(s){
+    return (s.match(/a/g) || []).length;
+}
+
 function repeatedString(s, n)
 {
-    const count = (s.match(/a/g) || []).length;
     const divisor = Math.floor(n / s.length);
     const remainder = n - (s.length * divisor);
 
-    return divisor * count + s.substr(0, remainder).length;
+    return divisor * countA(s) + countA(s.substr(0, remainder));
 }
 
 repeatedString('aba', 10);
 repeatedString('a', 1000000000000);
+repeatedString('ceebbcb', 817723);
+repeatedString('gfcaaaecbg', 547602);
 
 // 7
 // 1000000000000
+// 0
+// 164280
